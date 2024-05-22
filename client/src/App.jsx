ebe428d4b3de6a8de38311f3b34dbd0e5b45b34b
nitindahiya199 <nitindahiya00000@gmail.com>
2024-05-22 12:51:16 +0530

    setup routing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,21 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Home from "./pages/Home"
+import About from "./pages/About"
+import Contact from "./pages/Contact"
+import SignUp from "./pages/SignUp"
+import SignIn from "./pages/SignIn"
+
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home /> }/>
+      <Route path="/about-us" element={<About />} />
+      <Route path="/contact-us" element={<Contact />} />
+      <Route path="/sign-in" element={<SignIn />} />
+      <Route path="/sign-up" element={<SignUp />} />
+      <Route path="*" element={<Home />} />
+    </Routes>
+    </BrowserRouter>
   )
 }
