baeb4730c59e5e623c574d38a9b641d0437c181c
nitindahiya199 <nitindahiya00000@gmail.com>
2024-05-26 00:48:39 +0530

    connected to mongodb and add user model

--- api/index.js
+++ api/index.js
@@ -1,4 +1,16 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+mongoose
+  .connect(
+    process.env.MONGODB
+  )
+  .then(() => {
+    console.log(`database is connected`);
+  });
 
 const app = express();
 const port = 3000;
